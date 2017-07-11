0b1572ad6419b4fdbee6e899bc761dbced0ec027
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-07-11 17:38:53 -0400

    all tests for queue passing

--- src/queue.js
+++ src/queue.js
@@ -1,5 +1,20 @@
 class Queue {
-
+  constructor() {
+    this.queue = [];
+    this.size = 0;
+  }
+  size() {
+    return this.size;
+  }
+  enqueue(...args) {
+    this.queue.push(...args);
+    ++this.size;
+  }
+  dequeue() {
+    if (this.size === 0) return this.size;
+    --this.size;
+    return this.queue.shift();
+  }
 }
 
 module.exports = Queue;
