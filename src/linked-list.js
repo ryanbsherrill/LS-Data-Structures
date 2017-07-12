48eb82d3e1abbd2f08aa62c6bbf1c2806c05dbbe
Ryan Sherrill <ryanbsherrill@gmail.com>
2017-07-12 02:43:54 -0400

    all tests passing for linked-list

--- src/linked-list.js
+++ src/linked-list.js
@@ -4,15 +4,35 @@ class LinkedList {
     this.tail = null;
     // Do not modify anything inside of the constructor
   }
+  addToTail(value) {
+    const node = {
+      value,
+      next: null,
+    };
+    if (!this.head) {
+      this.head = node;
+    } else {
+      let thisNode = this.head;
+      while (thisNode.next) {
+        thisNode = thisNode.next;
+      }
+      thisNode.next = node;
+    }
+    this.tail = node;
+  }
+  removeHead() {
+    const offWithHisHead = this.head.value;
+    this.head = this.head.next;
+    return offWithHisHead;
+  }
+  contains(value) {
+    let now = this.head;
+    while (now.next) {
+      if (now.value === value) return true;
+      now = now.next;
+    }
+    return false;
+  }
 }
-/**
- * Should have the methods: `addToTail`, `removeHead`, and `contains`.
- * `addToTail` replaces the tail with a new value that is passed in.
- * `removeHead` removes and returns the head node.
- * `contains` should searth through the linked list and return true if a matching value is found.
- * The `head` property is a reference to the first node and the `tail` property is a reference to the last node.
- * These are the only two properties that you need to keep track of an infinite number of nodes.
- * Build your nodes with objects.
-*/
 
 module.exports = LinkedList;
